0a230b8b7b81a293355b530961212c1115c4d6ab
Fer Quagliaro <quagliarofernanda@gmail.com>
2020-04-14 10:29:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-var animData = {
-        container: document.getElementById('container'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'https://labs.nearpod.com/bodymovin/demo/ik/data.json'
-    };
-    var anim = bodymovin.loadAnimation(animData);;
-window.onresize = anim.resize.bind(anim);
+// Enabler Initialization
+// If true, start function. If false, listen for INIT.
+window.onload = function() {
+    if (Enabler.isInitialized()) {
+        enablerInitHandler();
+    } else {
+        Enabler.addEventListener(studio.events.StudioEvent.INIT, enablerInitHandler);
+    }
+}
+
+function enablerInitHandler() {
+    console.log(“dentro!”);
+}
+//Polite Load
+if (Enabler.isInitialized()) {
+    init();
+} else {
+    Enabler.addEventListener(studio.events.StudioEvent.INIT, init);
+}
+// Runs when Enabler is ready.
+function init() {
+    if (Enabler.isPageLoaded()) {
+        politeInit();
+    } else {
+        Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, politeInit);
+    }
+};
+// Runs when the page is completely loaded.
+function politeInit() {
+    // Add your code to hide any loading image or animation and load creative
+    // assets or begin creative animation.
+};
